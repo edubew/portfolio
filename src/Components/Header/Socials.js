d5de7176783c46216c994b5a3446ac954ffea18a
edubew <edubew98@gmail.com>
2022-11-29 04:26:11 +0300

    Fix linters errors

--- src/Components/Header/Socials.js
+++ src/Components/Header/Socials.js
@@ -8,9 +8,10 @@ const Socials = () => (
   <Div className="header__socials">
     <a
       href="https://www.linkedin.com/in/winfred-edube-9820a422a/"
-      target="_blank" 
-      rel="noreferrer">
-    <BsLinkedin />
+      target="_blank"
+      rel="noreferrer"
+    >
+      <BsLinkedin />
     </a>
     <a href="https://github.com/edubew" target="_blank" rel="noreferrer"><BsGithub /></a>
     <a href="https://twitter.com/edube_winne" target="_blank" rel="noreferrer"><BsTwitter /></a>
